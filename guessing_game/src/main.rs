b5dada0a38f3ef600d57da0afb1267289a18aada
death667b <idaniel@spinnys.com>
2022-08-14 22:58:04 +1000

    Finish the guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,41 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut try_number: u32 = 0;
+
+    loop {
+        try_number = try_number + 1;
+        println!("Please input your guess ({try_number}):");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please only enter numbers");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You Win!!");
+                println!("It took you {try_number} tries!");
+                break;
+            }
+        }
+    }
 }
