85dd3e7b9bf38a4aa7a30ed3f55041eaa9c1c671
death667b <idaniel@spinnys.com>
2022-08-29 11:56:39 +1000

    learn impl

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,43 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self, scale: u32) -> u32 {
+        self.width * self.height * scale
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self { width: size, height: size }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle {
+        width: 30, 
+        height: 50
+    };
+
+    let rect2 = Rectangle {
+        width: 10, 
+        height: 40
+    };
+
+    let rect3 = Rectangle {
+        width: 60, 
+        height: 45
+    };
+
+    let sq = Rectangle::square(4);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
