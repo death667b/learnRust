3908e4af429dbcfa32b9f6527d78659d6f7d374a
death667b <idaniel@spinnys.com>
2022-08-29 10:57:28 +1000

    struct User

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,26 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut user1 = build_user(
+        String::from("idaniel@spinnys.com"), 
+        String::from("death667b")
+    );
+
+    user1.email = String::from("ian@spinnys.com");
+
+    println!("{}", user1.username);
 }
