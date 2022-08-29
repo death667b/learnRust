60b3dfc401d9a41116d3ae2366b6492b341e4497
death667b <idaniel@spinnys.com>
2022-08-29 11:05:35 +1000

    start new rectangles

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -14,11 +14,19 @@ fn build_user(email: String, username: String) -> User {
     }
 }
 
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+struct AlwaysEqual;
+
 fn main() {
     let mut user1 = build_user(
         String::from("idaniel@spinnys.com"), 
         String::from("death667b")
     );
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    let subject = AlwaysEqual;
 
     user1.email = String::from("ian@spinnys.com");
 
